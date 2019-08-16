fef2f9afc818d4566945fabea59620072934973a
PaulSklv <paulsklvv@gmail.com>
2019-08-16 18:56:03 +0200

    Updates

---
+++ precache-manifest.43f13a11f23d435151cf15f6c34328d4.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9823e59d7d83fad98ccf10428dd3fd63",
+    "url": "/udemy-songs/index.html"
+  },
+  {
+    "revision": "408043c2b5e01f1262ad",
+    "url": "/udemy-songs/static/js/2.54769ba5.chunk.js"
+  },
+  {
+    "revision": "ec745d9c903b19a96880",
+    "url": "/udemy-songs/static/js/main.63eab2e0.chunk.js"
+  },
+  {
+    "revision": "fd2b13f639100b182fd9",
+    "url": "/udemy-songs/static/js/runtime~main.c4e762d9.js"
+  }
+]);
