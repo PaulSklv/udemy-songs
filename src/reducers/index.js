44d3cf24aabe663fb9aef0b2dfcdeec04672f08e
PaulSklv <paulsklvv@gmail.com>
2019-08-16 18:52:55 +0200

    added project

---
+++ src/reducers/index.js
@@ -0,0 +1,23 @@
+import { combineReducers } from 'redux';
+
+const songsReducer = () => {
+  return [
+    { title: 'No Scrubs', duration: '4:05' },
+    { title: 'Los', duration: '3:25' },
+    { title: 'Unsainted', duration: '4:37' },
+    { title: 'Du hast', duration: '3:56' }
+  ];
+};
+
+const selectedSongReducer = (selectedSong = null, action) => {
+  if (action.type === 'SONG_SELECTED') {
+    return action.payload;
+  }
+
+  return selectedSong;
+};
+
+export default combineReducers({
+  songs: songsReducer,
+  selectedSong: selectedSongReducer
+});
